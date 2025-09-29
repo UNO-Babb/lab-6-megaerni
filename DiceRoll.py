5e89091f22f125182b2e006e8feeee4ba7b58583
megaerni <165853906+megaerni@users.noreply.github.com>
2025-09-29 18:38:44 +0000

    started dice roll

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,18 +1,24 @@
 #DiceRoll.py
-#Name:
-#Date:
-#Assignment:
+#Name: Meg Aerni
+#Date: 9.29.2025
+#Assignment: Lab 6, Part 1
 import random
 
 def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
-  #find the sum total of the two dice
-  
+  for r in range(100):
+    dice1 = random.randint(1, 6)
+    rolls[dice1 - 1] = rolls[dice1 - 1] + 1
+  dice2 = random.randint(1, 6)
+  #find the sum total of the two dice 
+  dice = 1
+  for count in rolls:
+    print(dice, ":", count)
+    dice = dice + 1
   #print statictics for dice rolls
-
+  print(rolls)
 
 if __name__ == '__main__':
   main()
